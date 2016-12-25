34cfae81db898352a6afb54ccbb398f5c8335a91
Hoang Xuan Phu <phuhimself@phunehehe.net>
2016-12-25 18:30:25 +0800

    update android sdk

--- android-ci/install.sh
+++ android-ci/install.sh
@@ -11,15 +11,20 @@ $nixEnv --file /root --install
 ln --symbolic /nix/var/nix/profiles/default/lib /lib64
 
 
-for p in curl gnutar gzip
+for p in curl.bin unzip
 do
     PATH=$($nixBuild '<nixpkgs>' --attr $p)/bin:$PATH
 done
 mkdir --parents "$ANDROID_HOME"
-curl --silent "$SDK_URL" \
-| tar --directory "$ANDROID_HOME" --extract --gzip --strip-components=1
 
-echo y | "$ANDROID_HOME/tools/android" update sdk --no-ui --all \
+zip=sdk.zip
+curl --silent "$SDK_URL" --output $zip
+unzip $zip -d $ANDROID_HOME
+rm $zip
+
+
+echo y \
+| "$ANDROID_HOME/tools/android" update sdk --no-ui --all \
     --filter android-24,build-tools-24.0.1,platform-tools,extra-android-m2repository
 
 
